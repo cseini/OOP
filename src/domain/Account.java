efb9785fc0acacbb3387384879088279ed1ec68a
cseini <cseini@nate.com>
2018-06-14 10:17:28 +0900

    account 추가

--- src/domain/Account.java
+++ src/domain/Account.java
@@ -1,6 +1,7 @@
 package domain;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
 public class Account {
 	public final static String BANK_NAME="비트뱅크";
 	public final static String ACCOUNT_TYPE="기본통장";
@@ -9,9 +10,9 @@ public class Account {
 	public final static String DEPOSIT_SUCCESS="입금성공";
 	public final static String DEPOSIT_FAIL="적합한 입력값이 아님";
 	protected int money;
-	protected String name, uid, pass, createDate, accountNo;
-	Date date = new Date();
-	SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일");
+	protected String name, uid, pass, createDate, accountNo,randomNum;
+	Date date;
+	SimpleDateFormat sdf;
 	// 통장 123-345-678 의 형태가 되도록 코딩
 	// createDate는 블로그에서 오늘 날짜 뽑는 로직 가져다 쓰세요.
 	public void setMoney(int money) {
@@ -27,12 +28,18 @@ public class Account {
 		this.pass=pass;
 	}
 	public void setCreateDate() {
-		this.createDate=sdf.format(date);
+		date = new Date();
+		sdf = new SimpleDateFormat("yyyy년 MM월 dd일");
+		createDate=sdf.format(date);
+	}
+	public void setRandomNum() {
+		this.randomNum=String.format("%03d",(int)(Math.random()*1000));
 	}
 	public void setAccountNo() {
 		accountNo="";
 		for(int i=0;i<3;i++) {
-			accountNo += (i!=2)?String.format("%03d",(int)(Math.random()*1000))+"-":String.format("%03d",(int)(Math.random()*1000));
+			setRandomNum();
+			accountNo += (i!=2)?randomNum+"-":randomNum;
 		}
 	}
 	public int getMoney() {
@@ -52,6 +59,9 @@ public class Account {
 	}
 	public String getAccountNo() {
 		return accountNo;
+	}
+	public String getRandomNum() {
+		return randomNum;
 	}
 	public String toString() {
 		return String.format("%s \n%s \n계좌번호 %s \n이름 : %s \n생성일 : %s \n잔액 : %d원",BANK_NAME,ACCOUNT_TYPE,accountNo,name,createDate,money);
